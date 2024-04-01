35ae8c12615e546b0e9e2473a073dbd1ab35e8ec
AndreaDeBlasio95 <sviluppatoredeblasioandrea@gmail.com>
2024-04-01 23:35:47 +0200

    integration all rive's animations to test

--- lib/custom_widgets/my-rive-animation.dart
+++ lib/custom_widgets/my-rive-animation.dart
@@ -1,39 +1,65 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
 class RiveAnimationBottle extends StatefulWidget {
-  const RiveAnimationBottle({super.key});
-
   @override
-  State<RiveAnimationBottle> createState() => _RiveAnimationBottleState();
+  _RiveAnimationBottleState createState() => _RiveAnimationBottleState();
 }
 
 class _RiveAnimationBottleState extends State<RiveAnimationBottle> {
+  late SimpleAnimation _controller;
+  int _animationIndex = 0; // Track the current animation index
+
   @override
   void initState() {
-    // TODO: implement initState
+    super.initState();
+    // Initialize with the first animation
+    _controller = SimpleAnimation('1 - Idle');
+  }
+
+  void _toggleAnimation() {
+    setState(() {
+      // Increment the animation index with each tap
+      _animationIndex++;
+      // Reset the index if it exceeds the number of animations
+      if (_animationIndex > 3) _animationIndex = 0;
+
+      // Determine the animation based on the current index
+      switch (_animationIndex) {
+        case 0:
+          _controller = SimpleAnimation('1 - Idle');
+          break;
+        case 1:
+          _controller = SimpleAnimation('2 - Solo Bottle');
+          break;
+        case 2:
+          _controller = SimpleAnimation('3 - Open Bottle');
+          break;
+        case 3:
+          _controller = SimpleAnimation('4 - Pergamena');
+          break;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          height: 300,
-          width: 300,
-        ),
-        Container(
-          height: 400,
-          width: 400,
-          child: RiveAnimation.asset(
-            "animations/idle-loop-1.riv",
-            artboard: 'New Artboard',
-            animations: ['1 - Idle'],
-            fit: BoxFit.fitWidth,
+    return GestureDetector(
+      onTap: _toggleAnimation, // Change animation on tap
+      child: Stack(
+        children: [
+          Container(
+            height: 400,
+            width: 400,
+            child: RiveAnimation.asset(
+              "animations/animation-parchment.riv",
+              artboard: 'New Artboard',
+              controllers: [_controller], // Provide the current controller
+              fit: BoxFit.fitWidth,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
